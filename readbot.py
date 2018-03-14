c7c61c530a9a167adce4aa4283f0d659267d9e08
jgreelyBR <jason.greely@bloomreach.com>
2018-03-14 15:54:21 -0400

    empty checks and attribute check for text

--- readbot.py
+++ readbot.py
@@ -7,8 +7,20 @@ reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit(subName)
 
 for submission in subreddit.hot(limit=5):
-    print(submission.title)
-    print(submission.text)
-    print(submission.score)
+    title = submission.title
+    text = ""
+    if hasattr(submission, "text"):
+        text = submission.text
+
+    score = submission.score
+
+    if title:
+        print(title)
+
+    if text:
+        print(text)
+
+    if score:
+        print(score)
     print("======================\n")
 
